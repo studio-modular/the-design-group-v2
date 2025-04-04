d322796fee80c9178a2495e9aabbdb7c297d483d
Jack Jeffress <jack@modular.co>
2025-04-04 22:23:41 +1100

    Prepared for deploy

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,15 @@
-import * as migration_20250404_103452_initial from './20250404_103452_initial';
-import * as migration_20250404_103603_added_seo from './20250404_103603_added_seo';
+import * as migration_20250404_103452_initial from "./20250404_103452_initial";
+import * as migration_20250404_103603_added_seo from "./20250404_103603_added_seo";
 
 export const migrations = [
   {
-    up: migration_20250404_103452_initial.up,
     down: migration_20250404_103452_initial.down,
-    name: '20250404_103452_initial',
+    name: "20250404_103452_initial",
+    up: migration_20250404_103452_initial.up,
   },
   {
-    up: migration_20250404_103603_added_seo.up,
     down: migration_20250404_103603_added_seo.down,
-    name: '20250404_103603_added_seo'
+    name: "20250404_103603_added_seo",
+    up: migration_20250404_103603_added_seo.up,
   },
 ];
